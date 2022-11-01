18363605615f4f728bca41a66a779eb77ab21595
Kanaknakola <jakub.pecak@o2.pl>
2022-11-01 15:05:53 +0100

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './nav/navbar/navbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { QualificationComponent } from './qualification/qualification.component';
+import { ServicesComponent } from './services/services.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { ButtonUpComponent } from './button-up/button-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    SkillsComponent,
+    QualificationComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    ContactComponent,
+    ButtonUpComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/button-up/button-up.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-up',
+  templateUrl: './button-up.component.html',
+  styleUrls: ['./button-up.component.scss']
+})
+export class ButtonUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
