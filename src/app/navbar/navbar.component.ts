22e9f82c26ef54654cd5fe7c93f655108c79d7b4
Kanaknakola <jakub.pecak@o2.pl>
2022-11-01 15:55:57 +0100

    dark mode

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DarkModeService } from 'angular-dark-mode';
+
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  
+  darkMode$: Observable<boolean> = this.darkModeService.darkMode$;
 
-  constructor() { }
+  constructor(private darkModeService: DarkModeService) { }
 
   ngOnInit(): void {
+  }
+
+  onToggle(): void {
+    this.darkModeService.toggle();
   }
 
 }
